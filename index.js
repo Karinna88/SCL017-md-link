b3573fe6e74539f7c151fa1facbf0eceb97e7136
Karinna <karinna.sepulveda.matamala@gmail.com>
2021-08-30 09:18:06 -0400

    funciones para leer archivos y directorios, uso de metodos

--- index.js
+++ index.js
@@ -1,55 +1,70 @@
-
 // voy a usar expresiones regulares y require/module.exports**********
 
 //STAT: si el archivo es un directorio o un archivo, usandostats.isFile()ystats.isDirectory()
 
-const fs = require('fs');
+const fs = require("fs");
+const fetch = require('node-fetch');
 const ruta = process.argv[2];
 
-fs.stat(ruta,(err, stats) => {
-    if(err) {
-        console.log(err);
+fs.stat(ruta, (err, stats) => {
+  if (err) {
+    console.log(err);
+  } else {
+    if (stats.isFile()) {
+      //aqui se leera un archivo
+      mdArchivo(ruta);
     }
-else{
-    if(stats.isFile()){ //aqui se leera un archivo
-        mdArchivo(ruta);
+    if (stats.isDirectory()) {
+      mdDirectorio(ruta);
     }
-   if(stats.isDirectory()){
-        mdDirectorio(ruta);
-   }
-}
-})
+  }
+});
 
-const mdArchivo = (archivo)=>{
-   fs.readFile(archivo,"utf-8", (err, data) => {
+const mdArchivo = (archivo) => {
+  fs.readFile(archivo, "utf-8", (err, data) => {
+    const expRegular = /\[([^\]]+)]\((https?:\/\/[^\s)]+)\)/g; //exp.reg. para encontrar links
+    let iterador = data.matchAll(expRegular);
+    let linkList = [];
 
-    let expRegular = /\d/ig;
-    console.log(expRegular.test(data));
+    for (let url of iterador) {
+      linkList.push({ 
+        href: url[2],
+        text: url[1],
+        file: archivo, // aún esta relativo*********
+      });
+    }
 
-   })
-}
+    //esto debe ir en un promesa
 
-const mdDirectorio = (directorio)=>{
-    fs.readdir(directorio, (err, data) =>{
-   
-        data.forEach((archivo) =>{
-            if (archivo.includes(".md")) {
-                console.log(archivo);
-            }
-        })
-    })
-}
+    for (let link of linkList){
+           fetch(link.href)
+           .then(function(response) {
+             if (response.status === 200){
+                 console.log('ok');
+                 link.status = 200;
 
-
-
-
-
-
-
-
-
+             }  
+           })
+           .catch(function(err){
+             link.status = err.status;
+           })
+    }
 
 
+
+    console.log(linkList);
+  });
+};
+
+const mdDirectorio = (directorio) => {
+  fs.readdir(directorio, (err, data) => {
+    data.forEach((archivo) => {
+      if (archivo.includes(".md")) {
+        console.log(archivo);
+      }
+    });
+  });
+};
 
 
 
--- readmePruebas/readme2.md
+++ readmePruebas/readme2.md
@@ -1,2 +1,6 @@
-***hola
-![Diagrama](/images/diagrama.png)
+* [Path](https://nodejs.org/api/path.html)
+
+
+
+* [Leer un directorio](https://nodejs.org/api/fs.html#fs_fs_readdir_path_options_callback)
+
